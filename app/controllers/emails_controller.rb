c5e4dfa64a440deb9e07f38a5638b25ba8d7d5d1
lionelcoding <louis.lionel@gmail.com>
2021-11-26 00:21:04 +0100

    fonctionnalité update

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -16,7 +16,7 @@ class EmailsController < ApplicationController
 
   def show
     @email = Email.find(params[:id])
-    # @email.update(read: true)
+    @email.update(read: true)
     respond_to do |format|
       format.html { redirect_to root_path }
       format.js { }
@@ -25,11 +25,11 @@ class EmailsController < ApplicationController
 
   def update
     @email = Email.find(params[:id])
-    # if @email.read === true
-    #   @email.update(read: false)
-    # else 
-    #   @email.update(read: true)
-    # end
+    if @email.read === true
+      @email.update(read: false)
+    else 
+      @email.update(read: true)
+    end
   end
 
   def destroy
