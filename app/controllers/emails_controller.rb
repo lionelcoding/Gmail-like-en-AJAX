4de8eb995c26c778dad4a0d767b4bbf2ea08e368
lionelcoding <louis.lionel@gmail.com>
2021-11-25 23:11:57 +0100

    first

---
+++ app/controllers/emails_controller.rb
@@ -0,0 +1,44 @@
+require 'faker'
+
+class EmailsController < ApplicationController
+ 
+  def index
+    @emails = Email.all
+  end
+
+  def create
+    @email = Email.create(object: Faker::Movie.title, body: Faker::Movie.quote)
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.js { }
+    end
+  end
+
+  def show
+    @email = Email.find(params[:id])
+    # @email.update(read: true)
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.js { }
+    end
+  end
+
+  def update
+    @email = Email.find(params[:id])
+    # if @email.read === true
+    #   @email.update(read: false)
+    # else 
+    #   @email.update(read: true)
+    # end
+  end
+
+  def destroy
+    @email = Email.find(params[:id])
+    @email.destroy
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.js { }
+    end
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'emails/index'
+  devise_for :users
+  root to: "emails#index"
+  resources :tasks, except: [:show]
+  resources :emails
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
